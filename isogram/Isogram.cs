373f8f0259acd6417965b5346e56c9a83c09bd44
Victor H F <51718734+victorh1590@users.noreply.github.com>
2021-10-24 18:36:51 -0300

    Isogram exercise solution.

---
+++ isogram/Isogram.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+public static class Isogram
+{
+    public static bool IsIsogram(string word)
+    {
+        word = word.ToLower();
+        for (int chr = 0; chr < word.Length - 1; chr++)
+        {
+            if (char.IsWhiteSpace(word[chr]) 
+                || word[chr] == '-' 
+                || !char.IsLetter(word[chr]))
+                continue;
+            for (int nextChr = chr + 1; nextChr < word.Length; nextChr++)
+            {
+                if (word[nextChr] == word[chr])
+                    return false;
+            }
+        }
+
+        return true;
+    }
+}
