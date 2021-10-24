20ae5fceba9896da1f418df4e80541ad56339955
Victor H F <51718734+victorh1590@users.noreply.github.com>
2021-10-24 11:11:21 -0300

    Squeaky Clean exercise solution.

---
+++ squeaky-clean/SqueakyClean.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Linq;
+using System.Text;
+
+public static class Identifier
+{
+    public static string Clean(string identifier)
+    {
+        StringBuilder builder = new StringBuilder();
+        int[] greekLetters = Enumerable.Range('α', 'ω').ToArray();
+
+        for (int i = 0; i < identifier.Length; i++)
+        {
+            char chr = identifier[i];
+            
+            if ( Char.IsWhiteSpace(chr) )
+                builder.Append('_');
+            else if ( Char.IsControl(chr) )
+                builder.Append("CTRL");
+            else if (chr == '-')
+            {
+                if ( i + 1 < identifier.Length )
+                    builder.Append( Char.ToUpper( identifier[++i] ) );
+            }
+            else if ( Char.IsLetter(chr) && !( greekLetters.Contains(chr) ) || chr == '_')
+                builder.Append(chr);
+        }
+        
+        return builder.ToString();
+    }
+        
+}
