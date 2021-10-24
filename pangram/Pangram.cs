21bcad0800f74f2e892b119986c043ae4b21811d
Victor H F <51718734+victorh1590@users.noreply.github.com>
2021-10-24 14:05:17 -0300

    Pangram exercise solution.

---
+++ pangram/Pangram.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+public static class Pangram
+{
+    public static bool IsPangram(string input)
+    {
+        int element = 0;
+        char[] alphabet = Enumerable.Range('a', 26)
+            .Select(letter => (char)letter)
+            .ToArray();
+        input = input.ToLower();
+
+        if ( String.Empty == input || input.Length < 26 )
+            return false;
+        
+        foreach ( char chr in alphabet )
+        {
+            if( !input.Contains(chr) )
+                return false;
+        }
+
+        return true;
+    }
+}
