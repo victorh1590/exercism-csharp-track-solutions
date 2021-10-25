e6dd0a7c522001b206c36b387a6055272ea7afa8
Victor H F <51718734+victorh1590@users.noreply.github.com>
2021-10-25 11:06:22 -0300

    Reverse String exercise solution.

---
+++ reverse-string/ReverseString.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+public static class ReverseString
+{
+    public static string Reverse(string input)
+    {
+        StringBuilder builder = new StringBuilder();
+
+        for (int i = (input.Length - 1); i > -1 ; i--)
+        {
+            builder.Append(input[i]);
+        }
+
+        return builder.ToString();
+    }
+}
