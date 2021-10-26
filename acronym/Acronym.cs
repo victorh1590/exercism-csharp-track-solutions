f60d225585cc6da4e9518247a1e5dfec1bfc1969
Victor H F <51718734+victorh1590@users.noreply.github.com>
2021-10-26 17:34:56 -0300

    Acronym exercise solution.

---
+++ acronym/Acronym.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+public static class Acronym
+{
+    public static string Abbreviate(string phrase)
+    {
+        StringBuilder acronym = new StringBuilder();
+        var wordList = phrase.Split(' ').ToList();
+        wordList = wordList.Select(word => word.Trim()).ToList();
+        
+        foreach (string word in wordList)
+        {
+            const char APOSTROPHE = '\'';
+            bool letterAdded = false;
+            
+            if (String.IsNullOrEmpty(word))
+                continue;
+            foreach (var chr in word)
+            {
+                if (!Char.IsLetter(chr) && chr != APOSTROPHE)
+                {
+                    letterAdded = false;
+                    continue;
+                }
+
+                if (letterAdded)
+                    continue;
+
+                acronym.Append(Char.ToUpper(chr));
+                letterAdded = true;
+            }
+        }
+
+        return acronym.ToString();
+    }
+}
