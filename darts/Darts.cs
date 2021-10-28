73f801e918dc85a22303a413d4d0b3ebe1d9d8ec
Victor H F <51718734+victorh1590@users.noreply.github.com>
2021-10-27 21:24:41 -0300

    Darts exercise solution.

---
+++ darts/Darts.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using static System.Math;
+
+public static class Darts
+{
+    public static int Score(double x, double y)
+    {
+        double module = Sqrt(Pow(x, 2) + Pow(y, 2));
+
+        return module switch
+        {
+            <= 1 => 10,
+            <= 5 => 5,
+            <= 10 => 1,
+            _ => 0
+        };
+    }
+}
