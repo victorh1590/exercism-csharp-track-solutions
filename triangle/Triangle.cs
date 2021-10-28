5d4f7522ac1bb5838d9b1f6b4c6b87312486a7b8
Victor H F <51718734+victorh1590@users.noreply.github.com>
2021-10-27 21:58:12 -0300

    Triangle exercise solution.

---
+++ triangle/Triangle.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Linq;
+
+public static class Triangle
+{
+    private static bool TriangleInequality(params double[] sides)
+    {
+        double largerSide = sides.Max();
+        double sumOfSides = sides.Sum();
+        return largerSide < (sumOfSides - largerSide);
+    }
+    public static bool IsScalene(double side1, double side2, double side3)
+        => side1 != side2 &&
+           side1 != side3 &&
+           side2 != side3 &&
+           TriangleInequality(side1, side2, side3);
+
+    public static bool IsIsosceles(double side1, double side2, double side3)
+        => (side1 == side2 || 
+            side1 == side3 || 
+            side2 == side3 ) && 
+           TriangleInequality(side1, side2, side3);
+    
+    public static bool IsEquilateral(double side1, double side2, double side3)
+        => side1 == side2 && 
+           side1 == side3 && 
+           side2 == side3 && 
+           side1 != 0;
+}
