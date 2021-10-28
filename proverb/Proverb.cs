65104f1837902a1923cc3ac849b4dc280f5978d8
Victor H F <51718734+victorh1590@users.noreply.github.com>
2021-10-28 17:24:22 -0300

    Proverb exercise solution.

---
+++ proverb/Proverb.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+public static class Proverb
+{
+    public static string[] Recite(string[] subjects)
+    {
+        if (subjects.Length == 0)
+            return Array.Empty<string>();
+        
+        string proverb = "";
+
+        for (int i = 0; i < subjects.Length - 1; i++)
+        {
+            proverb += $"For want of a {subjects[i]} the {subjects[i+1]} was lost.&&";
+        }
+
+        proverb += $"And all for the want of a {subjects[0]}.";
+        return proverb.Split("&&");
+    }
+}
