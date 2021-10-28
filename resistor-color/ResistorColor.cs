cf9a4fbe33a2f98658b7ad9a5664e401f810cb63
Victor H F <51718734+victorh1590@users.noreply.github.com>
2021-10-28 15:54:33 -0300

    Resistor Color exercise solution.

---
+++ resistor-color/ResistorColor.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+public static class ResistorColor
+{
+    public static int ColorCode(string color)
+    {
+        string[] colors = Colors();
+        for (int i = 0; i < colors.Length; i++)
+        {
+            if (string.Equals(color, colors[i], StringComparison.CurrentCultureIgnoreCase))
+                return i;
+        }
+
+        return -1;
+    }
+
+    public static string[] Colors() 
+        => new [] 
+        { 
+            "black", "brown", "red", "orange", "yellow",
+            "green", "blue", "violet", "grey", "white"
+        };
+
+}
