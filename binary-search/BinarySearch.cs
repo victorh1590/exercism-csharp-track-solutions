22f3f86eaf7b73301c305c3f3cbc7fca267e0853
Victor H F <51718734+victorh1590@users.noreply.github.com>
2021-10-28 20:36:31 -0300

    Binary Search exercise solution.

---
+++ binary-search/BinarySearch.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+public static class BinarySearch
+{
+    public static int Find(int[] input, int value)
+    {
+        if (input.Length == 0) return -1;
+        Array.Sort(input);
+
+        int lowerLimit = 0;
+        int upperLimit = input.Length - 1;
+
+        while (true)
+        {
+            int middle = (lowerLimit + upperLimit) / 2;
+            
+            if (input[middle] == value) return middle;
+
+            if (upperLimit - lowerLimit <= 0) return -1;
+
+            if (value < input[middle])
+                upperLimit = middle - 1;
+            else
+                lowerLimit = middle + 1;
+        }
+    }
+}
