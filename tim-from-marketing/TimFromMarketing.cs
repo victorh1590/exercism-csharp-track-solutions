62868359d5ebc0f310f8c1bf67a82e0fbeea26c0
Victor H F <51718734+victorh1590@users.noreply.github.com>
2021-10-28 23:46:16 -0300

    Remove redundant logic.

--- tim-from-marketing/TimFromMarketing.cs
+++ tim-from-marketing/TimFromMarketing.cs
@@ -4,7 +4,7 @@ using System.Linq;
 static class Badge
 {
     public static string Print(int? id, string name, string? department)
-        => $"{ new string( id == null ? "" : $"[{id}] - " ) }" +
+        => (id == null ? "" : $"[{id}] - ") +
            $"{name} - " +
            $"{department?.ToUpper() ?? "OWNER"}";
 }
