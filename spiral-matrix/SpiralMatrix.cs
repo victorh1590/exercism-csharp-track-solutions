d205fa7ceb8261da88b70c97cf26ae893cf5e13c
Victor H F <51718734+victorh1590@users.noreply.github.com>
2021-10-30 18:57:59 -0300

    Spiral Matrix exercise solution.

--- spiral-matrix/SpiralMatrix.cs
+++ spiral-matrix/SpiralMatrix.cs
@@ -2,125 +2,61 @@
 
 public class SpiralMatrix
 {
-    // public delegate void produceColumnAndReversedLine(int size);
-    // public delegate void produceReversedColumnAndLine(int size);
     public static int[,] GetMatrix(int size)
     {
         if (size == 1) return new [,]{ {1} };
         
         int [,] matrix = new int[size,size];
         int counter = 1, line = 0, column = 0;
-
-        // int sizeDup = size - 2;
+        
         bool NumbersLeft() => counter <= (size * size);
+        
+        void Line()
+        {
+            while (column < size && matrix[line, column] == 0)
+                matrix[line, column++] = counter++;
+            column--;
+            line++;
+        }
+
+        void Column()
+        {
+            while (line < size && matrix[line, column] == 0)
+                matrix[line++, column] = counter++;
+            column--;
+            line--;
+        }
+
+        void ReversedLine()
+        {
+            while (column >= 0 && matrix[line, column] == 0)
+                matrix[line, column--] = counter++;
+            column++;
+            line--;
+        }
+
+        void ReversedColumn()
+        {
+            while (line >= 0 && matrix[line, column] == 0)
+                matrix[line--, column] = counter++;
+            line++;
+            column++;
+        }
 
         while (NumbersLeft())
         {
-            while (column < size && matrix[line, column] == 0)
-            {
-                matrix[line, column++] = counter++;
-            }
-
+            Line();
             if (!NumbersLeft()) break;
-            column--;
-            line++;
-
-            while (line < size && matrix[line, column] == 0)
-            {
-                matrix[line++, column] = counter++;
-            }
-
+            
+            Column();
             if (!NumbersLeft()) break;
-            column--;
-            line--;
-            while (column >= 0 && matrix[line, column] == 0)
-            {
-                matrix[line, column--] = counter++;
-            }
-
+            
+            ReversedLine();
             if (!NumbersLeft()) break;
-            column++;
-            line--;
-
-            while (line >= 0 && matrix[line, column] == 0)
-            {
-                matrix[line--, column] = counter++;
-            }
-
-            line++;
-            column++;
-            sizeDup--;
+            
+            ReversedColumn();
         }
-        
 
         return matrix;
     }
-    
-    
-    // produceColumnAndReversedLine operation1 = produceColumn;
-    //         produceColumnAndReversedLine operation2 = produceReversedLine;
-    //         produceColumnAndReversedLine combined = operation1 + operation2;
-    //
-    //         produceReversedColumnAndLine operation3 = produceReversedColumn;
-    //         produceReversedColumnAndLine operation4 = produceLine;
-    //         produceReversedColumnAndLine combined2 = operation3 + operation4;
-    //
-    //
-    //         void produceLine(int size)
-    //         {
-    //             while (column <= size - 1)
-    //             {
-    //                 if (matrix[line, ++column] != 0) break;
-    //                 matrix[line, column] = counter++;
-    //             }
-    //             line++;
-    //         }
-    //         
-    //         void produceColumn(int size)
-    //         {
-    //             while (line <= size - 1)
-    //             {
-    //                 if (matrix[++line, column] != 0) break;
-    //                 matrix[line, column] = counter++;
-    //             }
-    //             column--;
-    //         }
-    //
-    //         void produceReversedLine(int size)
-    //         {
-    //             while (column > 0)
-    //             {
-    //                 if (matrix[line, --column] != 0) break;
-    //                 matrix[line, column] = counter++;
-    //             }
-    //             line--;
-    //         }
-    //         
-    //         void produceReversedColumn(int size)
-    //         {
-    //             while (line > 0)
-    //             {
-    //                 if (matrix[--line, column] != 0) break;
-    //                 matrix[line, column] = counter++;
-    //             }
-    //             column++;
-    //         }
-    //
-    //         produceLine(size);
-    //
-    //         bool flip_flop = false;
-    //
-    //         for (int sizeDup = size; sizeDup > 0; sizeDup--)
-    //         {
-    //             if (!flip_flop)
-    //             {
-    //                 combined.Invoke(--sizeDup);
-    //                 flip_flop = true;
-    //                 continue;
-    //             }
-    //
-    //             combined2.Invoke(--sizeDup);
-    //             flip_flop = false;
-    //         }
-
 }
