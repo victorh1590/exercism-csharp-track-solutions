260badf5043ff92785d66375a1aad7790e44fdaf
Victor H F <51718734+victorh1590@users.noreply.github.com>
2021-10-30 20:47:41 -0300

    Scrabble Score exercise solution.

--- scrabble-score/ScrabbleScore.cs
+++ scrabble-score/ScrabbleScore.cs
@@ -4,35 +4,43 @@ using System.Linq;
 
 public static class ScrabbleScore
 {
-    private static Dictionary<int, HashSet<char>> _scoreTable = new()
+    private static Dictionary<char, int> _scoreTable = new();
+    private static int[] _scores = { 1,4,3,2,8,10,5 };
+    private static HashSet<char>[] _groups =
     {
-        [1] = new HashSet<char> {'A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S', 'T'},
-        [4] = new HashSet<char> {'F', 'H', 'V', 'W', 'Y'},
-        [3] = new HashSet<char> {'B', 'C', 'M', 'P'},
-        [2] = new HashSet<char> {'D', 'G'},
-        [8] = new HashSet<char> {'J', 'X'},
-        [10] = new HashSet<char> {'Q', 'Z'},
-        [5] = new HashSet<char> {'K'},
+        new() {'A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S', 'T'},
+        new() {'F', 'H', 'V', 'W', 'Y'},
+        new() {'B', 'C', 'M', 'P'},
+        new() {'D', 'G'},
+        new() {'J', 'X'},
+        new() {'Q', 'Z'},
+        new() {'K'}
     };
-    
-    private static int DigitValue(char chr)
+
+    private static void BuildDictionary()
     {
-        foreach (var (key, set) in _scoreTable)
-            if (set.Contains(chr)) return key;
-        return 0;
+        int i = 0;
+        foreach (var set in _groups)
+        {
+            foreach (var chr in set)
+                _scoreTable[chr] = _scores[i];
+            i++;
+        }
     }
-    
-    private static int CountCharacter(string input, char chr) => 
-        input.Count(c => c == chr);
+        
+    private static int DigitValue(char chr) => _scoreTable.ContainsKey(chr) ? _scoreTable[chr] : 0;
+
+    private static int CountCharacter(string input, char chr) => input.Count(c => c == chr);
 
     public static int Score(string input)
     {
         int sum = 0;
         input = input.ToUpper().Trim();
+        if(_scoreTable.Count == 0) BuildDictionary();
 
         while (input.Length > 0)
         {
-            sum += (CountCharacter(input, input[0]) * DigitValue(input[0]));
+            sum += CountCharacter(input, input[0]) * DigitValue(input[0]);
             input = input.Replace(input[0].ToString(), string.Empty);
         }
         return sum;
