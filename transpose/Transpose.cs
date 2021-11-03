20c1a4703f75cf12c9d9f7d3390ab7ddeaa2d3ce
Victor H F <51718734+victorh1590@users.noreply.github.com>
2021-11-03 20:07:40 -0300

    Transpose exercise solution.

--- transpose/Transpose.cs
+++ transpose/Transpose.cs
@@ -7,62 +7,39 @@ public static class Transpose
 {
     public static string String(string input)
     {
-        List<string> lines = input.SplitLines();
-        lines.Normalize();
+        List<string> lines = input.SplitLines().Normalize();
+        List<string> transposed = BuildList(lines);
+        StringBuilder transposedString = new();
+        foreach (string line in lines)
+        { 
+            for(int index = 0; index < line.Length; index++)
+                transposed[index] += line[index];
+        }
+        foreach (var line in transposed) transposedString.Append(line + '\n');
+        return transposedString.Length > 0 ? transposedString.ToString()[..^1] : transposedString.ToString();
+    }
+
+    private static List<string> BuildList(List<string> lines)
+    {
         List<string> transposed = new();
-
-        int height = lines.Height();
-        
-        for(int x = 0; x < height; x++) transposed.Add("");
-        
-        foreach(string line in lines)
-        {
-            for(int i = 0; i < line.Length; i++)
-            {
-                transposed[i] += line[i];
-            }
-        }
-
-        string result = "";
-        foreach (var line in transposed)
-        {
-            result += line;
-            result += '\n';
-        }
-
-        result = result.Trim(' ');
-        return result.Length > 0 ? result[..^1] : result;
+        for (int line = 0; line < lines.MaxLineWidth(); line++) transposed.Add("");
+        return transposed;
     }
 
     private static List<string> Normalize(this List<string> lines)
     {
-        int height = lines.Height();
-
-        int lastLength = lines[0].Length;
+        int lastLineLength = lines.Last().Length;
         
-        for(int i = 0; i < lines.Count; i++)
+        for (int line = lines.Count - 2; line >= 0; line--)
         {
-            if (lines[i].Length < height )
-            {
-                string spaces = new string(' ', (height - lines[i].Length));
-                lines[i] += spaces;
-            }
+            int currentLineLength = lines[line].Length;
+            if (currentLineLength< lastLineLength)
+                lines[line] += new string(' ', lastLineLength - currentLineLength);
+            else lastLineLength = currentLineLength;
         }
-
         return lines;
     }
-
-    private static List<string> SplitLines(this string input)
-        => input.Split('\n').ToList();
-
-    private static int Height(this List<string> input)
-    {
-        int max = 0;
-        foreach (var line in input)
-        {
-            if (line.Length > max) max = line.Length;
-        }
-
-        return max;
-    }
+    private static List<string> SplitLines(this string input) => input.Split('\n').ToList();
+    private static int MaxLineWidth(this List<string> input) 
+        => input.Select(line => line.Length).Prepend(0).Max();
 }
