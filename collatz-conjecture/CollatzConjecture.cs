53fb58394416fff5aa089aeaa3560a3db2e74295
Victor H F <51718734+victorh1590@users.noreply.github.com>
2021-11-05 18:56:28 -0300

    Collatz Conjecture exercise solution.

---
+++ collatz-conjecture/CollatzConjecture.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+public static class CollatzConjecture
+{
+    private static bool IsZero(int number) => number == 0;
+    private static bool IsOne(int number) => number == 1;
+    private static bool IsPositive(int number) => number > 0;
+    private static bool IsEven(int number) => number % 2 == 0;
+
+    public static int Steps(int number)
+    {
+        int steps = 0;
+
+        if (IsZero(number) || !IsPositive(number)) 
+            throw new ArgumentOutOfRangeException($"Invalid number: {number}.");
+
+        while (!IsOne(number))
+        {
+            if (IsEven(number)) number /= 2;
+            else number = (3 * number) + 1;
+            steps++;
+        }
+        return steps;
+    }
+}
