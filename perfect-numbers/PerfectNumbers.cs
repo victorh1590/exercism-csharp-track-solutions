a380450af739518162d2e6866cefc6c9284ed44f
Victor H F <51718734+victorh1590@users.noreply.github.com>
2021-11-08 18:08:48 -0300

    Perfect Numbers exercise solution.

---
+++ perfect-numbers/PerfectNumbers.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public enum Classification
+{
+    Perfect,
+    Abundant,
+    Deficient
+}
+
+public static class PerfectNumbers
+{
+    private static List<long> Factors(int number)
+    {
+        List<long> factors = new();
+        checked
+        {
+            for (long candidate = 1; candidate < number; candidate++)
+                if (number % candidate == 0) factors.Add(candidate);
+        }
+        return factors;
+    }
+
+    private static void ValidateNumber(int number)
+    {
+        if (number <= 0) throw new ArgumentOutOfRangeException("Invalid Number.");
+    }
+    
+    public static Classification Classify(int number)
+    {
+        ValidateNumber(number);
+        long sum = Factors(number).Sum();
+
+        if (sum == number) return Classification.Perfect;
+        if (sum < number) return Classification.Deficient;
+        if (sum > number) return Classification.Abundant;
+        throw new ArgumentOutOfRangeException("Invalid Number.");
+    }
+}
