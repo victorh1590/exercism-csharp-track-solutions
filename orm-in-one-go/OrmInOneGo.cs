9b09e53f5c624a8596741c707f8ae85f390ef54a
Victor H F <51718734+victorh1590@users.noreply.github.com>
2021-11-08 21:02:11 -0300

    Orm In One Go exercise solution.

---
+++ orm-in-one-go/OrmInOneGo.cs
@@ -0,0 +1,37 @@
+using System;
+
+public class Orm
+{
+    private Database database;
+
+    public Orm(Database database)
+    {
+        this.database = database;
+    }
+
+    public void Write(string data)
+    {
+        using (database)
+        {
+            database.BeginTransaction();
+            database.Write(data);
+            database.EndTransaction();
+        }
+    }
+
+    public bool WriteSafely(string data)
+    {
+        using var dbResource = database;
+        try
+        {
+            database.BeginTransaction();
+            database.Write(data);
+            database.EndTransaction();
+            return true;
+        }
+        catch
+        {
+            return false;
+        }
+    }
+}
