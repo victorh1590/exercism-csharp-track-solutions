1005ac1bf110327a08221f5a55a3ae0f0405090d
Victor H F <51718734+victorh1590@users.noreply.github.com>
2021-11-08 21:35:02 -0300

    Armstrong Numbers exercise solution.

---
+++ armstrong-numbers/ArmstrongNumbers.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+
+public static class ArmstrongNumbers
+{
+    public static bool IsArmstrongNumber(int number)
+    {
+        string digits = number.ToString();
+        int length = digits.Length;
+        int sum = 
+            digits.Sum(chr => Convert.ToInt32(Math.Pow(char.GetNumericValue(chr), length)));
+        return sum == number;
+    }
+}
