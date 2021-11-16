6b2fec8b29c3c3e5237c67fc125f583298b1c55c
Victor H F <51718734+victorh1590@users.noreply.github.com>
2021-11-16 01:01:22 -0300

    DnD Character exercise solution.

---
+++ dnd-character/DndCharacter.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+
+public class DndCharacter
+{
+    private static Random Die { get; set; }
+    public int Strength { get; }
+    public int Dexterity { get; }
+    public int Constitution { get; }
+    public int Intelligence { get; }
+    public int Wisdom { get; }
+    public int Charisma { get; }
+    public int Hitpoints { get; }
+
+    private DndCharacter()
+    {
+        Die = new Random();
+        Strength = Ability();
+        Dexterity = Ability();
+        Constitution = Ability();
+        Intelligence = Ability();
+        Wisdom = Ability();
+        Charisma = Ability();
+        Hitpoints = 10 + Modifier(Constitution);
+    }
+
+    public static int Modifier(int score) => (int)Math.Floor((score - 10) / 2.0);
+
+    public static int Ability()
+    {
+        int[] rolls = new int[4].Select(roll => Die.Next(1, 7)).ToArray();
+        return rolls.Sum() - rolls.Min();
+    }
+    
+    public static DndCharacter Generate() => new();
+}
