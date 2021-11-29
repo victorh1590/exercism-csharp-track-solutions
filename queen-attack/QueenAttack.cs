601af0aa889ad4bad9e22b306c1c38e6e3bae8b5
Victor H F <51718734+victorh1590@users.noreply.github.com>
2021-11-28 22:54:51 -0300

    Queen Attack exercise solution.

---
+++ queen-attack/QueenAttack.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+public class Queen
+{
+    public Queen(int row, int column)
+    {
+        Row = row;
+        Column = column;
+    }
+
+    public int Row { get; }
+    public int Column { get; }
+}
+
+public static class QueenAttack
+{
+    public static bool CanAttack(Queen white, Queen black) 
+        => white.Row == black.Row || 
+           white.Column == black.Column || 
+           white.Row + white.Column == black.Row + black.Column ||
+           Math.Abs(white.Row - white.Column) == Math.Abs(black.Row - black.Column);
+
+    public static Queen Create(int row, int column)
+    {
+        if (row is > 7 or < 0 || column is > 7 or < 0) throw new ArgumentOutOfRangeException();
+        return new Queen(row, column);
+    }
+}
