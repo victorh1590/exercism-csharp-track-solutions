ffc0266519e243c0927e43baaceeece95b8b543a
Victor H F <51718734+victorh1590@users.noreply.github.com>
2021-11-28 21:46:10 -0300

    Sum of Multiples exercise solution.

---
+++ sum-of-multiples/SumOfMultiples.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public static class SumOfMultiples
+{
+    public static int Sum(IEnumerable<int> multiples, int max)
+    {
+        HashSet<int> selected = new();
+        
+        foreach (var num in multiples)
+        {
+            if (num == 0) continue;
+            for (int i = 1; i < max; i++)
+            {
+                if (i % num == 0) selected.Add(i);
+            }
+        }
+
+        return selected.Sum();
+    }
+}
