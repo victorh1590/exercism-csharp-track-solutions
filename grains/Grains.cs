1061992c491ad47f5224562b7ff0cd9b52e5266e
Victor H F <51718734+victorh1590@users.noreply.github.com>
2021-12-03 05:11:51 -0300

    Grains exercise solution.

---
+++ grains/Grains.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Numerics;
+
+public static class Grains
+{
+    private const int NumSquares = 64;
+    private const int BaseNum = 2;
+    private static BigInteger SumOfSquares(int numSquares, ulong baseNum) 
+        => (BigInteger.Pow(baseNum, numSquares) - 1 / (baseNum - 1));
+    private static readonly Lazy<BigInteger> SumAllSquares = new(SumOfSquares(NumSquares, BaseNum));
+
+    public static ulong Square(int n)
+    {
+        if (n is <= 0 or > 64) throw new ArgumentOutOfRangeException();
+        return (ulong)Math.Pow(2, n-1);
+    }
+    public static ulong Total() => (ulong)SumAllSquares.Value;
+}
