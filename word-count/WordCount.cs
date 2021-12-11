3a2181b94357195be059db62ea1a7ceefeefc034
Victor H F <51718734+victorh1590@users.noreply.github.com>
2021-12-10 21:20:52 -0300

    Word Count exercise solution.

---
+++ word-count/WordCount.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+public static class WordCount
+{
+    public static IDictionary<string, int> CountWords(string phrase)
+    {
+        Dictionary<string, int> histogram = new();
+
+        Regex pattern = new Regex(
+            @"([0-9]||[A-z])(\w|\')+", 
+            RegexOptions.Compiled | RegexOptions.IgnoreCase);
+
+        var matches = pattern
+            .Matches(phrase)
+            .Select(match => match.ToString().ToLowerInvariant().Trim('\''));
+        
+        foreach (var word in matches)
+        {
+            if (histogram.ContainsKey(word)) histogram[word]++;
+            else histogram.Add(word, 1);
+        }
+
+        return histogram;
+    }
+}
