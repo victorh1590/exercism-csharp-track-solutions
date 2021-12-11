a36a94abecc6b98719aa5e8dbd800447565dbd23
Victor H F <51718734+victorh1590@users.noreply.github.com>
2021-12-10 23:30:05 -0300

    Refactor Circular Buffer.

--- circular-buffer/CircularBuffer.cs
+++ circular-buffer/CircularBuffer.cs
@@ -1,30 +1,21 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 
 public class CircularBuffer<T>
 {
     private object?[] Buffer;
     private int Begin { get; set; }
     private int End { get; set; }
-    private int _capacity { get; init; }
-    
+    private int Capacity { get; }
     private bool Full { get; set; }
-
     private bool Empty { get; set; }
 
     public CircularBuffer(int capacity)
     {
-        _capacity = capacity;
+        Capacity = capacity;
         Begin = 0;
         End = 0;
-        // Buffer = new List<T?>(capacity);
         Buffer = new object?[capacity];
-        for (int i = 0; i < capacity; i++)
-        {
-            Buffer[i] = null;
-        }
-        
+        for (int i = 0; i < capacity; i++) Buffer[i] = null;
         if (capacity != 0)
         {
             Empty = true;
@@ -38,12 +29,8 @@ public class CircularBuffer<T>
     }
 
     private int CycleIncrement(int currentPosition) 
-        => currentPosition + 1 < _capacity ? currentPosition + 1 : 0;
+        => currentPosition + 1 < Capacity ? currentPosition + 1 : 0;
     
-    // private bool BufferIsEmpty() => End == Begin;
-    //
-    // private bool BufferIsFull() => Math.Abs(End - Begin) == _capacity + 1;
-
     private void SetFullWhenWrite()
     {
         Empty = false;
@@ -66,19 +53,18 @@ public class CircularBuffer<T>
             SetEmptyWhenRead();
             return (T) result!;
         }
-        throw new InvalidOperationException();
+        else throw new InvalidOperationException();
     }
 
     public void Write(T? value)
     {
-        if (!Full)
+        if(!Full)
         {
             Buffer[End] = value;
             End = CycleIncrement(End);
             SetFullWhenWrite();
-            return;
         }
-        throw new InvalidOperationException();
+        else throw new InvalidOperationException();
     }
 
     public void Overwrite(T? value)
